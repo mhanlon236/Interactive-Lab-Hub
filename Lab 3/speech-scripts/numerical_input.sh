1de957db4cf6d5850569e8bf365d82b017376c3f
mhanlon236 <49703163+mhanlon236@users.noreply.github.com>
2023-09-25 22:25:20 -0400

    Add lab files

---
+++ Lab 3/speech-scripts/numerical_input.sh
@@ -0,0 +1,8 @@
+#https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
+
+#!/bin/bash
+say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
+#say $*
+say "Hey beautiful, what is your phone number?"
+
+python -u test_microphone.py -m en | python handle_test_microphone.py > numerical_input.txt
